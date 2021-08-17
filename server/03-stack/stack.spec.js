a69c675df9ce8f60b1bf3875f9b687e4f59c356a
Vicente Guzmán <vicente.g.guzman@accenture.com>
2021-08-17 10:22:43 -0500

    stack size is zero when pushed and popped

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,16 +1,15 @@
 const stackFactory = () => {
-  let empty = true;
   let count = 0;
 
-
   return {
-    isEmpty: () => empty,
+    isEmpty: () => count === 0,
     size: () => count,
     push: () => {
-      empty = false;
       count += 1;
     },
-    pop: () => {  empty = true; }
+    pop: () => {  
+      count -= 1;
+    }
   }
 };
 
@@ -51,7 +50,11 @@ describe('a stack', () => {
     expect(stack.isEmpty()).toBe(true);
   });
 
-  it.todo('stack size is 0 when pushed and popped');
+  it('stack size is 0 when pushed and popped', () => {
+    stack.push();
+    stack.pop();
+    expect(stack.size()).toBe(0);
+  });
 
   it.todo('throws overflow error when pushing to a stack at full capacity');
 
